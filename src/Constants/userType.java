35f79a20a1eb0f95fbd81b16f94242cacd698f74
ronak <ronakgupta.official@gmail.com>
2023-01-19 18:00:07 +0530

    Creating Constants

---
+++ src/Constants/userType.java
@@ -0,0 +1,8 @@
+package Constants;
+public class userType {
+    private userType(){};
+    public static final String USER = "user";
+    public static final String EDITOR = "editor";
+    public static final String CHIEF_EDITOR = "chiefeditor";
+
+}
---
+++ src/manager/bookMarkManager.java
@@ -0,0 +1,49 @@
+package manager;
+
+import Entity.Book;
+import Entity.Movie;
+import Entity.webLink;
+
+public class bookMarkManager {
+    private static bookMarkManager instance = new bookMarkManager();
+    private bookMarkManager(){}
+    public static bookMarkManager getInstance(){
+        return instance;
+    }
+    public Movie createMovie(long id, String title, String profileURL, int releaseYear,String[] cast,String[] direction,String genre, double imbdRating){
+        Movie movie = new Movie();
+        movie.setId(id);
+        movie.setTitle(title);
+        movie.setProfileURL(profileURL);
+        movie.setReleaseYear(releaseYear);
+        movie.setCast(cast);
+        movie.setDirection(direction);
+        movie.setGenre(genre);
+        movie.setImbdRating(imbdRating);
+        return movie;
+    }
+
+    public Book createBook(long id, String title, String profileURL, int publicationYear, String publisher, String[] author, String genre, double amazonRating){
+        Book book = new Book();
+        book.setId(id);
+        book.setTitle(title);
+        book.setPublicationYear(publicationYear);
+        book.setPublisher(publisher);
+        book.setAuthor(author);
+        book.setGenre(genre);
+        book.setAmazonRating(amazonRating);
+        return book;
+    }
+
+    public webLink createLink(long id, String title, String profileURL,String url,String host){
+        webLink link = new webLink();
+        link.setId(id);
+        link.setTitle(title);
+        link.setUrl(url);
+        link.setHost(host);
+        return link;
+    }
+
+
+
+}
