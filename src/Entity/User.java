5ea4be651344b1ad98794bc0d7a9245acf906ebf
ronak <ronakgupta.official@gmail.com>
2023-01-19 16:09:30 +0530

    Created User File

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -2,5 +2,68 @@ package Entity;
 
 public class User {
     private long id;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFisrtname() {
+        return fisrtname;
+    }
+
+    public void setFisrtname(String fisrtname) {
+        this.fisrtname = fisrtname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    private String password;
+    private String fisrtname;
+    private String lastname;
+    private String username;
+    private int gender;
 
 }
