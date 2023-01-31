33deec8ac3e25fccec20214ef9b338059298c071
ronak <ronakgupta.official@gmail.com>
2023-01-31 11:35:53 +0530

    Create shareable and kid friendly Status marked by

--- src/Controllers/BookmarkController.java
+++ src/Controllers/BookmarkController.java
@@ -5,13 +5,24 @@ import Entity.User;
 import manager.bookMarkManager;
 
 public class BookmarkController {
-    private BookmarkController(){}
+    private BookmarkController() {
+    }
+
     private static BookmarkController instance = new BookmarkController();
-    public static BookmarkController getInstance(){
+
+    public static BookmarkController getInstance() {
         return instance;
     }
 
     public void saveUserBookmark(User user, Bookmark bookmark) {
         bookMarkManager.getInstance().saveUserBookmark(user, bookmark);
+    }
+
+    public void setKidFriendlyStatus(User user, String kidFriendlyStatus, Bookmark bookmark) {
+        bookMarkManager.getInstance().setkidFriendlyStatus(user, kidFriendlyStatus, bookmark);
+    }
+
+    public void share(User user, Bookmark bookmark) {
+        bookMarkManager.getInstance().share(user, bookmark);
     }
 }
